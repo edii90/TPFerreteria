0b55be803000d5d83c28c951ed34f9d664ce4b05
edii90 <pipidamb@hotmail.com>
2014-09-15 22:48:54 -0300

    historial Compras completo

--- src/java/Datos/Dcompras.java
+++ src/java/Datos/Dcompras.java
@@ -45,7 +45,7 @@ public class Dcompras extends coneccionBD {
             while (rows.next()) {
                 Usuarios usr = dusr.traerXid(rows.getInt("idUsuario"));
                 Compras aux = new Compras(rows.getInt("idCompras"),usr, rows.getDate("fecha"));
-
+                aux.setTotal(rows.getFloat("total"));
                 lista.put(aux.getId(), aux);
             }
             return lista;
@@ -59,6 +59,7 @@ public class Dcompras extends coneccionBD {
     public Hashtable TraerLineasComprasPorIdCabecera(int idCabecera) throws Exception
     {
         try{
+            super.conectar();
             Hashtable aux = new Hashtable();
                 String sqlProd = "SELECT idLinea,idProd,nombre,precioUnit,cantidad FROM prodxcomp pc inner join productos p on p.idProductos=pc.idProd where idCompra =" + idCabecera + ";";
                 PreparedStatement psProd = Sentencia(sqlProd);
@@ -81,43 +82,20 @@ public class Dcompras extends coneccionBD {
         try {
             super.conectar();
             Dusuarios dusr = new Dusuarios();
-            Hashtable aux = new Hashtable();
-
-            String sql = "SELECT * FROM Compras where idUsuario = '" + user.getId() + ";";
-            PreparedStatement ps = Sentencia(sql);
-            ResultSet rows = consulta(ps);
-
-            while (rows.next()) {
-                    //int idLin,int idProd,String nombre, float CostoUnit, int Cantidad
-                    LineaDeCompra pro = new LineaDeCompra(rows.getInt("idLinea"), rows.getInt("idProd"), rows.getString("nombre"), rows.getFloat("precioUnit"),rows.getInt("cantidad"));
-                    aux.put(pro.getIdLinea(), pro);
-                }
-            return aux;
-        } catch (SQLException ex) {
-            throw new SQLException("Error en traer todas las Compras por usuario " + ex.getMessage());
-        } finally {
-            super.desconectar();
-        }
-    }
-
-    public Hashtable TraerComprasXusr(int id) throws Exception {
-        try {
-            super.conectar();
-            Dusuarios dusr = new Dusuarios();
             Hashtable lista = new Hashtable();
 
-            String sql = "SELECT * FROM Compras where idUsuario = '" + id + ";";
+            String sql = "SELECT * FROM Compras where idUsuario = '" + user.getId() + "';";
             PreparedStatement ps = Sentencia(sql);
             ResultSet rows = consulta(ps);
 
             while (rows.next()) {
-                    //int idLin,int idProd,String nombre, float CostoUnit, int Cantidad
-                    LineaDeCompra pro = new LineaDeCompra(rows.getInt("idLinea"), rows.getInt("idProd"), rows.getString("nombre"), rows.getFloat("precioUnit"),rows.getInt("cantidad"));
-                    lista.put(pro.getIdLinea(), pro);
+                Compras aux = new Compras(rows.getInt("idCompras"),user, rows.getDate("fecha"));
+                aux.setTotal(rows.getFloat("total"));
+                lista.put(aux.getId(), aux);
                 }
             return lista;
         } catch (SQLException ex) {
-            throw new SQLException("Error en traer todas las Compras por usuario por id " + ex.getMessage());
+            throw new SQLException("Error en traer todas las Compras por usuario " + ex.getMessage());
         } finally {
             super.desconectar();
         }
--- src/java/Servlets/HistorialCompra.java
+++ src/java/Servlets/HistorialCompra.java
@@ -7,6 +7,7 @@ package Servlets;
 
 import Datos.Dcompras;
 import Modelo.Compras;
+import Modelo.Usuarios;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
@@ -38,24 +39,34 @@ public class HistorialCompra extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, Exception {
-        
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession(true);
-        
+
         try {
             RequestDispatcher mw = request.getRequestDispatcher("MarcoWeb");
             mw.include(request, response);
-            
+
             if (session.getAttribute("Mensaje") != null) {
                 out.println("<div class='txtmsgerror'>" + session.getAttribute("Mensaje") + "</div>");
                 session.removeAttribute("Mensaje");
             }
+            
             Hashtable ListaCompras;
+            
             if (session.getAttribute("ListadoCompras") == null) {
-                Dcompras dcom = new Dcompras();
-                ListaCompras = dcom.TraerTodasCabacerasCompras();
-                session.setAttribute("ListadoCompras", ListaCompras);
+
+                if (((Usuarios) session.getAttribute("User")).getTipoUsr() == 1) {
+                    Dcompras dcom = new Dcompras();
+                    ListaCompras = dcom.TraerTodasCabacerasCompras();
+                    session.setAttribute("ListadoCompras", ListaCompras);
+                }else{
+                    
+                    Dcompras dcom = new Dcompras();
+                    ListaCompras = dcom.TraerComprasXusr((Usuarios) session.getAttribute("User"));
+                    session.setAttribute("ListadoCompras", ListaCompras);
+                }
             } else {
                 ListaCompras = (Hashtable) session.getAttribute("ListadoCompras");
             }
@@ -85,15 +96,17 @@ public class HistorialCompra extends HttpServlet {
                         + "	<div class='celdaHistorial'>$ " + aux.getTotal() + "</div>"
                         + "	<div class='celdaHistorial'><button class='botonVerHistorial'>ver</button></div>"
                         + "</div>");
+                formid++;
             }
             RequestDispatcher mwc = request.getRequestDispatcher("MarcoWebPie");
             mwc.include(request, response);
         } catch (Exception ex) {
-            session.setAttribute("Mensaje", "Error in catch" + ex);
+            session.setAttribute("Mensaje", "Error in catch " + ex);
         } finally {
             out.close();
         }
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
